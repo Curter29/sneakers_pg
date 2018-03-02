3ff302fed1160defe3cd19dc9782436b48a98e49
Aleksey Sudakov <curter29@gmail.com>
2018-03-02 16:24:41 +0300

    add rescue callback

--- lib/sneakers_pg.rb
+++ lib/sneakers_pg.rb
@@ -3,11 +3,15 @@ module Sneakers
     def do_work(delivery_info, metadata, msg, handler)
         super(delivery_info, metadata, msg, handler)
       rescue PG::ConnectionBad, PG::UnableToSend => e
+        work_rescue(e)
         logger.error(e)
         # Подождать какое то время, чтобы не плодить битые воркеры пока переключается haproxy
         sleep 2
         # Убиваем этот процесс, мастер процесс породит новый уже с нормальным соединением
         Process.kill('TERM', Process.pid)
+    end
+
+    def work_rescue(e)
     end
   end
 end
